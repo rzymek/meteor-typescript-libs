130850bb7890e8f1c716c0e4c1c70f353595c10a
Olivier Refalo <orefalo@yahoo.com>
2013-09-19 21:24:30 -0400

    add ironrouter

---
+++ ironrouter.d.ts
@@ -0,0 +1,75 @@
+// mrt add iron-router
+
+declare module  Router {
+
+	interface TemplateConfig {
+		to:string;
+	}
+
+	interface TemplateConfigDico {[id:string]:TemplateConfig}
+
+	interface Configuration {
+		layout: string;
+		notFoundTemplate: string;
+		loadingTemplate: string;
+		renderTemplates:TemplateConfigDico;
+
+		before?:Function;
+		after?:Function;
+	}
+
+//	interface RouteHandler {
+//		route(name:string, routeParams?:MapConfig);
+//		params:any[];
+//	}
+
+	interface MapConfig {
+		path:string;
+		// by default template is the route name, this field is the override
+		template?:string;
+
+		// can be a Function or an object literal {}
+		data?: any;
+		// waitOn can be a subscription handle, an array of subscription handles or a function that returns a subscription handle
+		// or array of subscription handles. A subscription handle is what gets returned when you call Meteor.subscribe
+		waitOn?: any;
+		loadingTemplate?:string;
+		notFoundTemplate?: string;
+		controller?: string;
+		action?: string;
+	}
+
+	// These are for Router
+	function page():void;
+
+	function add(route:Object):void;
+
+	function to(path:string, ...args:any[]):void;
+
+	function filters(filtersMap:Object);
+
+	function filter(filterName:string, options?:Object);
+
+	// These are for Iron-Router
+
+	function configure(config:Configuration);
+
+	function map(func:Function):void;
+
+	function route(name:string, routeParams?:MapConfig, handler?:any);
+
+	function path(route:string, params?:Object):void;
+
+	function url(route:string):void;
+
+	function go(route:string):void;
+
+//	routes: Object;
+//
+//	params: any[];
+
+}
+
+interface RouteController {
+
+}
