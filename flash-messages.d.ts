439877e18b55c74465b414e1a5e1110f783f83f4
Dave Allen <david.allen@meliortechnology.com>
2013-10-21 15:40:26 -0700

    Added definitions for flash-messages smart package, which seems inclusive of and superior to errors smart package

---
+++ flash-messages.d.ts
@@ -0,0 +1,18 @@
+// Definitions for the flash-messages smart package
+//
+// https://atmosphere.meteor.com/package/flash-messages
+// https://github.com/camilosw/flash-messages
+
+declare module FlashMessages {
+  function sendAlert(message: string, options?: ConfigurationOptions): void;
+  function sendError(message: string, options?: ConfigurationOptions): void;
+  function sendSuccess(message: string, options?: ConfigurationOptions): void;
+  function sendInfo(message: string, options?: ConfigurationOptions): void;
+  function clear(): void;
+  function configure(options: ConfigurationOptions): void;
+
+  interface ConfigurationOptions {
+    autoHide?: boolean;
+    hideDelay?: number;
+  }
+}
