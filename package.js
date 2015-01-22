52e6e693ddd68c309f4f78059b7e32a1fd18be68
Dave Allen <david.allen@meliortechnology.com>
2015-01-22 15:20:07 -0800

    Upped version to 1.0.4

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
     name: 'meteortypescript:typescript-libs',
     summary: 'Common TypeScript definition files for Meteor, including meteor.d.ts and much more.',
-    version: '1.0.3',
+    version: '1.0.4',
     git: 'https://github.com/meteor-typescript/meteor-typescript-libs'
 });
 
